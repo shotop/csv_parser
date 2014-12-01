e5463f8bebe0e5afbd833d7874d882b0640311eb
shotop <shotop@thoughtworks.com>
2014-12-01 12:53:15 -0600

    shotop: adds test data; preprocesses input files to deal with different delimiters

--- file_sorter.rb
+++ file_sorter.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class FileSorter
   attr_reader :arguments
 
@@ -7,5 +9,21 @@ class FileSorter
 
   def arguments_valid?
     @arguments.length == 3 ? true : false
+  end
+
+  def preprocess_input(input_file)
+    headers = File.open(input_file).first
+    if headers =~ /[,]/
+      separator = ","
+    elsif headers =~ /[|]/
+      separator = "|"
+    else
+      separator = " "
+    end
+
+    processed_csv = CSV.read(input_file, { :col_sep => separator })
+    processed_csv.shift #remove headers
+
+    return processed_csv
   end
 end
--- spec/file_sorter_spec.rb
+++ spec/file_sorter_spec.rb
@@ -25,4 +25,39 @@ describe "file_sorter" do
       end
     end
   end
+
+  describe '#preprocess_input' do
+
+    let(:file_sorter) {FileSorter.new(valid_args)}
+    let(:comma_separated_data) {valid_args[0]}
+    let(:pipe_separated_data) {valid_args[1]}
+    let(:space_separated_data) {valid_args[2]}
+
+    context 'processing input data with various delimiters' do
+
+      it 'parses comma-separated data' do
+        result = [["Hotop", "Tom", "Male", "Blue", "09/25/1971"]]
+
+        processed_csv = file_sorter.preprocess_input(comma_separated_data)
+
+        expect(processed_csv).to eq(result)
+      end
+
+      it 'parses pipe-separated data' do
+        result = [["Coca", "Andrea", "Female", "Blue", "02/25/1983"]]
+
+        processed_csv = file_sorter.preprocess_input(pipe_separated_data)
+
+        expect(processed_csv).to eq(result)
+      end
+
+      it 'parses space-separated data' do
+        result = [["Gerard", "Tom", "Male", "Blue", "02/26/1983"]]
+
+        processed_csv = file_sorter.preprocess_input(space_separated_data)
+
+        expect(processed_csv).to eq(result)
+      end
+    end
+  end
 end
